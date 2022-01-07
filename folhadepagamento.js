f18ece8efbdfd18eb78d7b9c862bd2ac47707645
Juliana <61429635+jumozaga@users.noreply.github.com>
2022-01-07 12:36:54 -0300

    Create folhadepagamento.js

---
+++ folhadepagamento.js
@@ -0,0 +1,7 @@
+let valor1 = parseInt(gets());
+let valor2 = parseInt(gets());
+let valor3 = parseFloat(gets());
+
+// Escreva o seu código aqui
+console.log("NUMBER = " + valor1);
+console.log("SALARY = U$ " + ((valor2*valor3).toFixed(2)));
