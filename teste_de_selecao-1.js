7fe100323348dd6e04503444a9bf72463faf5380
Juliana <61429635+jumozaga@users.noreply.github.com>
2022-01-11 13:18:12 -0300

    Update teste_de_selecao-1.js
    Com as condições corretas

--- teste_de_selecao-1.js
+++ teste_de_selecao-1.js
@@ -8,7 +8,7 @@ c = parseInt(x[2]);
 
 d = parseInt(x[3]);
 
-if (((b>c) && ((c+d)>=0)) && ((c && d)>=0)  && (a%2 == 0))
+if ( (b>c) && (d>a) && ((c+d)> (a+b)) && (c >=0) && (d >=0) && (a%2==0)  )
 {
 print("Valores aceitos");
 }else
